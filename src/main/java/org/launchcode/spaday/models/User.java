042f1bf30c146e2f51a17d21abb32654f0af6c28
vy3191 <venkyafhs@gmail.com>
2022-02-22 17:46:22 -0500

    completed adding validations

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,12 +1,27 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotEmpty(message = "Username is required.")
+    @Size(min = 3, max = 12, message = "User name mut be between 3 and 12 characters long")
     private String username;
+
+    @Email(message = "Please provide a valid email")
     private String email;
+
+    @NotEmpty(message = "Password is required.")
+    @Size(min = 6, message = "Passwords must be at least 6 characters long")
     private String password;
 
-    public User() {
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
+    public User() {
     }
 
     public User(String username, String email, String password) {
@@ -38,5 +53,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword(){
+        if ((this.password != null || this.verifyPassword != null) && !this.password.equals(this.verifyPassword)){
+            this.verifyPassword = null;
+        }
     }
 }
